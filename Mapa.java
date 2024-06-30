5e6de60568d1cc4680db7802637fe4d16ffb81aa
Paulo Almeida <estpaulovictora@live.com>
2024-06-30 20:04:32 -0300

    alteracao teste

--- Mapa.java
+++ Mapa.java
@@ -1,5 +1,5 @@
 public class Mapa {
     public static void main(String[] args) {
-        System.out.println("Este é o meu mapa, meu RA: 21052533-5.");
+        System.out.println("Este é o meu mapa, meu RA: 21052533-5. altera");
     }
 }
